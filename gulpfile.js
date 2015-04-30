964b6e7f0e8e0afab9ea281fc73ee982b5bd031c
Samuel Moritz <samuel.moritz@gmail.com>
2015-04-30 21:13:10 +0200

    Add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,24 @@
 var gulp = require('gulp');
 
+var files_3pp = ['src/main/js_3pp/*.js','src/main/css_3pp/*.css'];
+var files = ['src/main/js/*.js','src/main/js/*.html'];
+var all_files = files.concat(files_3pp);
+
 gulp.task('3pp', function () {
-    gulp.src(['src/main/js_3pp/*.js','src/main/css_3pp/*.css'])
+    gulp.src(files_3pp)
         .pipe(gulp.dest('build/resources/main/static'))
 });
 
 gulp.task('js', function () {
-    gulp.src(['src/main/js/*.js','src/main/js/*.html'])
+    gulp.src(files)
         .pipe(gulp.dest('build/resources/main/static'))
 });
 
 gulp.task('build', ['3pp', 'js']);
 
 gulp.task('default', ['build']);
+
+gulp.task('watch', function () {
+    gulp.watch(all_files, ['build']);
+});
+
